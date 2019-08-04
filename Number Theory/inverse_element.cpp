a6b65d7378ec289f7dfc4ef8948ff3a0fe3e0ca3
Little Prince <tjw199723@163.com>
2019-08-04 19:19:07 +0800

    Update inverse_element.cpp

--- Number Theory/inverse_element.cpp
+++ Number Theory/inverse_element.cpp
@@ -1,3 +1,4 @@
+/*线性递推求出小于模数p的元素的逆元*/
 inv[1] = 1;
 for(int i = 2; i < p; ++ i) //p为模数
-    inv[i] = (p - p / i) 1ll** inv[p % i] % p; // care overflow
+    inv[i] = (p - p / i) *1ll* inv[p % i] % p; // care overflow
